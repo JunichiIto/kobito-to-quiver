bb99161272a0bfb381367db2743d15d363174f8e
Junichi Ito <jit@sonicgarden.jp>
2017-12-31 08:31:02 +0900

    Refactoring

--- lib/kobito_item.rb
+++ lib/kobito_item.rb
@@ -3,56 +3,63 @@ require 'active_support/core_ext/integer'
 require 'active_support/core_ext/time'
 
 class KobitoItem
+  ITEM_SQL = <<~SQL.freeze
+    SELECT
+      Z_PK,
+      ZTITLE,
+      ZIN_TRASH,
+      ZRAW_BODY,
+      ZCREATED_AT,
+      ZUPDATED_AT,
+      ZUPDATED_AT_ON_QIITA,
+      ZURL
+    FROM
+      ZITEM
+    WHERE
+      ZIN_TRASH IS NULL
+    ORDER BY
+      ZUPDATED_AT DESC
+  SQL
+
+  TAG_SQL = <<~SQL.freeze
+    SELECT ZNAME
+    FROM ZITEM
+      INNER JOIN Z_2TAGS
+        ON ZITEM.Z_PK = Z_2TAGS.Z_2ITEMS
+      INNER JOIN ZTAG
+        ON Z_2TAGS.Z_3TAGS = ZTAG.Z_PK
+    WHERE
+      ZITEM.Z_PK = ?
+  SQL
+
   attr_accessor :pk, :raw_created_at, :raw_updated_at, :raw_body, :title, :url, :tags
 
   class << self
     def extract_items
-      sql = <<~SQL
-        SELECT
-          Z_PK,
-          ZTITLE,
-          ZIN_TRASH,
-          ZRAW_BODY,
-          ZCREATED_AT,
-          ZUPDATED_AT,
-          ZUPDATED_AT_ON_QIITA,
-          ZURL
-        FROM
-          ZITEM
-        WHERE
-          ZIN_TRASH IS NULL
-        ORDER BY
-          ZUPDATED_AT DESC
-      SQL
-      run_query(sql).map { |row|
-        item = KobitoItem.new
-        item.pk = row['Z_PK']
-        item.raw_created_at = row['ZCREATED_AT']
-        item.raw_updated_at = row['ZUPDATED_AT']
-        item.raw_body = row['ZRAW_BODY']
-        item.title = row['ZTITLE']
-        item.url = row['ZURL']
-        find_tags(item.pk) do |tag|
-          item.tags << tag
-        end
+      run_query(ITEM_SQL).map { |row|
+        item = to_kobito_item(row)
         [item.pk, item]
       }.to_h
     end
 
     private
 
+    def to_kobito_item(row)
+      item = KobitoItem.new
+      item.pk = row['Z_PK']
+      item.raw_created_at = row['ZCREATED_AT']
+      item.raw_updated_at = row['ZUPDATED_AT']
+      item.raw_body = row['ZRAW_BODY']
+      item.title = row['ZTITLE']
+      item.url = row['ZURL']
+      find_tags(item.pk) do |tag|
+        item.tags << tag
+      end
+      item
+    end
+
     def find_tags(pk)
-      sql = <<~SQL
-        SELECT ZNAME
-        FROM ZITEM
-          INNER JOIN Z_2TAGS
-            ON ZITEM.Z_PK = Z_2TAGS.Z_2ITEMS
-          INNER JOIN ZTAG
-            ON Z_2TAGS.Z_3TAGS = ZTAG.Z_PK
-        WHERE
-          ZITEM.Z_PK = ?
-      SQL
-      run_query(sql, pk).each do |row|
+      run_query(TAG_SQL, pk).each do |row|
         yield row['ZNAME']
       end
     end
@@ -68,9 +75,10 @@ class KobitoItem
     end
 
     def db
-      @db ||= SQLite3::Database.new(db_path).tap do |_db|
-        _db.results_as_hash = true
-      end
+      @db ||=
+        SQLite3::Database.new(db_path).tap do |_db|
+          _db.results_as_hash = true
+        end
     end
 
     def db_path
@@ -98,10 +106,12 @@ class KobitoItem
     # 先頭行をカット
     regexp = /\A[^\n]+\n+/
     _body = raw_body.sub(regexp, '')
+
     # Qiitaリンク追加
-    unless url.nil?
+    if qiita_published?
       _body = "Qiita: #{url}\n\n#{_body}"
     end
+
     _body
   end
 
