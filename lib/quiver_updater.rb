5ebcdf4e74d5899e78a4ff1e3fd84329644c0ce2
Junichi Ito <jit@sonicgarden.jp>
2017-12-30 18:03:16 +0900

    Create quiver updater

---
+++ lib/quiver_updater.rb
@@ -0,0 +1,73 @@
+require 'pathname'
+require 'json'
+require '../lib/kobito_item'
+
+class QuiverUpdater
+  NOTEBOOK_DIR = '/Users/jit/Desktop/6AB38D2B-32F6-47BE-9F46-96A6E6997E02.qvnotebook'
+  NOTEBOOK_DIR = File.expand_path('../../qvnotebook', __FILE__)
+  def run
+    pattern = File.join(NOTEBOOK_DIR, '**/*.qvnote')
+    Dir.glob(pattern).each do |dir|
+      update_meta_json(dir)
+      update_content_json(dir)
+    end
+  end
+
+  private
+
+  def update_meta_json(dir)
+    json_path = File.join(dir, 'meta.json')
+    json_data = open(json_path) do |io|
+      JSON.load(io)
+    end
+    id = json_data['title']
+    puts "updating meta.json / #{id}"
+    if id !~ /^\d+$/
+      puts 'skip.'
+      return
+    end
+    item = kobito_items[id.to_i]
+    if item.nil?
+      puts 'not found.'
+      return
+    end
+    json_data['title'] = item.title
+    json_data['created_at'] = item.created_at.to_i
+    json_data['updated_at'] = item.updated_at.to_i
+    json_data['tags'] = item.tags
+    write_json(json_path, json_data)
+  end
+
+  def update_content_json(dir)
+    json_path = File.join(dir, 'content.json')
+    json_data = open(json_path) do |io|
+      JSON.load(io)
+    end
+    id = json_data['title']
+    puts "updating content.json / #{id}"
+    if id !~ /^\d+$/
+      puts 'skip.'
+      return
+    end
+    item = kobito_items[id.to_i]
+    if item.nil?
+      puts 'not found.'
+      return
+    end
+    json_data['title'] = item.title
+    write_json(json_path, json_data)
+  end
+
+  def write_json(json_path, json_data)
+    # p json_data
+    open(json_path, 'w') do |io|
+      io.write(JSON.pretty_generate(json_data))
+    end
+  end
+
+  def kobito_items
+    @kobito_items ||= KobitoItem.extract_items
+  end
+end
+
+QuiverUpdater.new.run
